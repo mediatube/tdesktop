37f8adfb2521372d4910355739dcd30742b7e84a
SlavikMIPT <slavikmipt@gmail.com>
2019-10-18 18:28:15 +0300

    drag media view x2 scale on middle button fullscreen on photo opening

--- Telegram/SourceFiles/media/view/media_view_overlay_widget.h
+++ Telegram/SourceFiles/media/view/media_view_overlay_widget.h
@@ -157,6 +157,7 @@ private:
 	void mouseDoubleClickEvent(QMouseEvent *e) override;
 	void mouseMoveEvent(QMouseEvent *e) override;
 	void mouseReleaseEvent(QMouseEvent *e) override;
+	void resizeEvent(QResizeEvent *e) override;
 	void contextMenuEvent(QContextMenuEvent *e) override;
 	void touchEvent(QTouchEvent *e);
 
@@ -367,10 +368,12 @@ private:
 	int _x = 0, _y = 0, _w = 0, _h = 0;
 	int _xStart = 0, _yStart = 0;
 	int _zoom = 0; // < 0 - out, 0 - none, > 0 - in
+	int _scale = 0;
 	float64 _zoomToScreen = 0.; // for documents
 	QPoint _mStart;
 	bool _pressed = false;
 	int32 _dragging = 0;
+	int32 _scaling = 0;
 	QPixmap _current;
 	bool _blurred = true;
 
@@ -416,7 +419,7 @@ private:
 
 	OverState _over = OverNone;
 	OverState _down = OverNone;
-	QPoint _lastAction, _lastMouseMovePos;
+	QPoint _lastAction, _lastMouseMovePos, _windowPos;
 	bool _ignoringDropdown = false;
 
 	Ui::Animations::Basic _stateAnimation;
